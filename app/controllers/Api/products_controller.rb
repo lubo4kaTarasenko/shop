59e7ec4ee4cb66f9792d756509f6a342d0017206
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-13 19:14:57 +0200

    filter on products list

--- app/controllers/Api/products_controller.rb
+++ app/controllers/Api/products_controller.rb
@@ -1,12 +1,30 @@
 class Api::ProductsController < ActionController::API
   def index
-    products_filtred =  Product.search(params[:q]).includes(:category)
-    pages_count = (products_filtred.count / 24).ceil
-    products = Product.search(params[:q]).includes(:category).page(params[:p]).per(24).map do |p| 
+    scp = get_list
+    products = scp.page(params[:p]).per(24).map do |p| 
       p.attributes.merge( 
         category: p.category.name
       ) 
     end
+
+    pages_count = ((scp.count) / 24).ceil
     render json: { products: products, pages: pages_count }
+  end
+
+  def get_list
+    scp = Product.search(params[:q]).includes(:category)
+    
+    if params[:f] == 'A...Z'
+      scp = scp.order(name: :asc)
+    elsif params[:f] == 'Z...A'
+      scp = scp.order(name: :desc)
+    elsif params[:f] == 'cheap...expensive'
+      scp = scp.order(price: :asc)
+    elsif params[:f] == 'expensive...cheap'
+      scp = scp.order(price: :desc)
+    else
+      scp
+    end 
+    scp
   end
 end
