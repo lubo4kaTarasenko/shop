dfd9e9b2e3a04ea16ce7edbda126d742c10f6a06
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-13 12:29:47 +0200

    return pages count

--- app/controllers/Api/products_controller.rb
+++ app/controllers/Api/products_controller.rb
@@ -1,10 +1,12 @@
 class Api::ProductsController < ActionController::API
   def index
-    products =  Product.search(params[:q]).includes(:category).page(params[:p]).map do |p| 
+    products_filtred =  Product.search(params[:q]).includes(:category)
+    pages_count = (products_filtred.count / 24).ceil
+    products = Product.search(params[:q]).includes(:category).page(params[:p]).per(24).map do |p| 
       p.attributes.merge( 
         category: p.category.name
       ) 
     end
-    render json: { products: products }
+    render json: { products: products, pages: pages_count }
   end
 end
