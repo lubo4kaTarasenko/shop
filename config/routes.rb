491e18e7faf241ba4e8af0df52b4f2334f6f1f53
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-14 20:37:44 +0200

    correct routes order

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  root = 'home#index'
-  root to: root
+  get '/products', to: 'api/products#index'
+  get '/product', to: 'api/products#show'
+  get '/categories', to: 'api/categories#index'
 
-  get '/', to: root
+  root = 'home#index'
+
+  root to: root
   get '/:Warriors', to: root
   get '/:Unforgiven/:Two', to: root
   get '/:Back/:In/:Black', to: root
   get '/:Born/:To/:Be/:Wild', to: root
   get '/:Rock/:To/:Like/:a/:Hurricane', to: root
   get '/:I/:Was/:Made/:For/:Lovin/:You', to: root  
-
-  get '/products', to: 'api/products#index'
-  get '/product', to: 'api/products#show'
-  get '/categories', to: 'api/categories#index'
 end
