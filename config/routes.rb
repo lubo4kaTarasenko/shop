b4e26b2afb313836b3fa6ff7c59419024d520392
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-14 20:28:04 +0200

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root = 'home#index'
+  root to: root
+
+  get '/:Warriors', to: root
+  get '/:Unforgiven/:Two', to: root
+  get '/:Back/:In/:Black', to: root
+  get '/:Born/:To/:Be/:Wild', to: root
+  get '/:Rock/:To/:Like/:a/:Hurricane', to: root
+  get '/:I/:Was/:Made/:For/:Lovin/:You', to: root  
+
   get '/products', to: 'api/products#index'
   get '/product', to: 'api/products#show'
   get '/categories', to: 'api/categories#index'
