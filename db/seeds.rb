788976c837e930413f71fb81ce66b53e9b8af5c5
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-24 22:20:49 +0200

    fix seed syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,5 +67,5 @@ products = 1000.times.map do
       description: Faker::Markdown.emphasis
     }
   )
-
+end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
