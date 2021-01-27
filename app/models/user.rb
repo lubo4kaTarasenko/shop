cc126d8880a8efbfebf31410b1cdc04d1dc8f8b9
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-27 23:25:19 +0200

    user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,11 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    Rails.logger.info(auth.inspect)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
+      user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
       user.password_confirmation = user.password
     end
