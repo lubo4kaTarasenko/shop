bf48bb03947d232cb68df188020ee16ca96814d3
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2021-01-30 23:09:26 +0200

    github secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, '1f22a753eddd53f34bf4', ENV['GITHUB_SECRET'], scope: 'user:email'
+  config.omniauth :github, 'd672c65431ee52477a2f', ENV['GITHUB_SECRET'], scope: 'user:email'
   OmniAuth.config.allowed_request_methods << :get
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
