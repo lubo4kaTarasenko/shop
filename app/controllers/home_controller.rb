ef4acf4fc8df61f6d517efafde63e912e2ac5b20
luba <lubo4ka.boiko@gmail.com>
2021-04-13 19:08:57 +0300

    add webhook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+
+Logger.info(params.inspect)
+Logger.info(params)
+
     html = File.read(Rails.root.join('public/index.html').to_s)
     render html: html.html_safe
   end
